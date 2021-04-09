368f4e6f363896c0da44ea7227992093ed1c8f60
Mateus bezzano <mateusb@goatme.app>
2021-04-09 15:39:55 -0300

    Termino do Desafio

---
+++ Dom/desafio-01/app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: "#desafio",
+  data: {
+    nome: "Mateus Correia Bezzan",
+    idade: 24,
+    linkImagem:
+      "https://exame.com/wp-content/uploads/2016/09/size_960_16_9_sol14.jpg?quality=70&strip=info",
+  },
+  methods: {
+    multiplicaIdade: function () {
+      return this.idade * 3;
+    },
+    numeroAleatorio: function () {
+      return Math.random() * (1 - 0) + 0;
+    },
+    inicializarNome (event) {
+        this.nome = event.target.value
+    }
+  },
+});
